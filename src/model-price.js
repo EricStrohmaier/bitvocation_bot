1dd2aa51021bf136e92fbadf18afc509d86e22e6
EricStrohmaier <eric.strohmaier00@gmail.com>
2023-12-23 08:27:29 +0100

    removed js files again

--- src/model-price.js
+++
@@ -1,6 +0,0 @@
-export const MODEL_PRICES = {
-    'text-davinci-003': 0.00002,
-    'text-curie-001': 0.000002,
-    'text-babbage-001': 0.0000005,
-    'text-ada-001': 0.0000004,
-};
--- src/translation.js
+++
@@ -1,2 +0,0 @@
-import fs from 'fs';
-export const TRANSLATIONS = JSON.parse(fs.readFileSync('./translations.json').toString());
