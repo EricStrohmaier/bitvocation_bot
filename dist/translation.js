e322d5c0b942cc6bc85e1cafba89146d3b21c743
Eric Strohmaier <eric.strohmaier00@gmail.com>
2024-04-25 19:14:09 +0200

    change translation

--- dist/translation.js
+++ dist/translation.js
@@ -5,4 +5,5 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.TRANSLATIONS = void 0;
 const fs_1 = __importDefault(require("fs"));
-exports.TRANSLATIONS = JSON.parse(fs_1.default.readFileSync('./translations.json').toString());
+const path_1 = __importDefault(require("path"));
+exports.TRANSLATIONS = JSON.parse(fs_1.default.readFileSync(path_1.default.join(__dirname, "translations.json"), "utf8"));
--- src/translation.ts
+++ src/translation.ts
@@ -1,37 +1,40 @@
-import fs from 'fs';
+import fs from "fs";
+import path from "path";
 
-export const TRANSLATIONS: {
-    [key: 'en' | 'de' | string]: {
-      general: {
-        'language-switch': string;
-        'start-message': string;
-        'donate': string;
-        'btc-price': string;
-        'latest-jobs': string;
-        'help': string;
-        'query-keywords': string;
-        'categories': string;
-        'job-alert': string;
-      };
-      'command-descriptions': {
-        language: string;
-        start: string;
-        donate: string;
-        checkprice: string;
-        jobs: string;
-        help: string;
-        jobalert: string
-        categories: string;
-        freeguide: string;
-        privacy: string;
-      };
-      errors: {
-        'generic-error': string;
-        'image-safety': string;
-        'no-parameter-command': string;
-        'invalid-language': string;
-      };
+export type Translations = {
+  [key in "en" | "de" | string]: {
+    general: {
+      "language-switch": string;
+      "start-message": string;
+      donate: string;
+      "btc-price": string;
+      "latest-jobs": string;
+      help: string;
+      "query-keywords": string;
+      categories: string;
+      "job-alert": string;
     };
-  } = JSON.parse(fs.readFileSync('./translations.json').toString());
-  
-  
+    "command-descriptions": {
+      language: string;
+      start: string;
+      donate: string;
+      checkprice: string;
+      jobs: string;
+      help: string;
+      jobalert: string;
+      categories: string;
+      freeguide: string;
+      privacy: string;
+    };
+    errors: {
+      "generic-error": string;
+      "image-safety": string;
+      "no-parameter-command": string;
+      "invalid-language": string;
+    };
+  };
+};
+
+export const TRANSLATIONS: Translations = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "translations.json"), "utf8")
+);
